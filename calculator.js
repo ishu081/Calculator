514389a2a6e9ce7509014e907e0841e66aed0385
ishu <shresthaishu222@gmail.com>
2025-04-20 19:24:58 +0545

    initial commit

---
+++ calculator.js
@@ -0,0 +1,65 @@
+*{
+    padding: 0;
+    margin: 0;
+  }
+  body{
+  background-color: darkgray;
+  overflow: unset;
+  }
+  
+  .topic{
+    text-align: center;
+    margin-top: 3rem;
+        font-size: 6rem;
+  
+  }
+  
+  
+  .container{
+    border: 1px solid gray;
+    border-radius: 1rem;
+    height: 30rem;
+    margin: 11rem 43rem;
+    width: 33rem;
+    background-color: black;
+  
+  
+  }
+  
+  .container1{
+    border: 1px solid black;
+    border-radius: 1rem;
+    height: 8rem;
+    width: 33rem;
+    margin-top: 0.5rem;
+    background-color: white;
+    color: black;
+  
+  
+  }
+  .container2{
+    border: 1px solid black;
+    border-radius: 1rem;
+    height: 17rem;
+    width: 33rem;
+    margin-top: 0.5rem;
+    background-color: black;
+    margin-top: 4rem;
+    color: black;
+  
+  
+  }
+  
+  button{
+    width: 6rem;
+    height: 3rem;
+    margin: 0.2rem 0.19rem;
+  
+  }
+  
+  #r2{
+    font-size: 1rem;
+  }
+  
+  
+  
